45fc6fc3892d65272bfd934f911f8066ce95fe0a
Martin Michlmayr <tbm@cyrius.com>
2020-12-20 14:12:44 +0800

    Make more adjustments to find corresponding audio file

--- common.py
+++ common.py
@@ -13,8 +13,12 @@ def get_audio(text):
     """
 
     text = text.split(',')[0]
+    text = text.split(';')[0]
     text = text.split(' (')[0]
+    if text.startswith('-a '):
+        text = text[3:]
     text = text.lstrip('-')
+    text = text.rstrip('-')
     text = text.replace(' ', '_')
     audio = CACHE.expanduser() / f"Sw-ke-{text}.opus"
     if audio.exists():
